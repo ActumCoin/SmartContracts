c62366022007f68b315ea57fc83d039a50deed38
rshanedev <ryansshane@rocketmail.com>
2018-02-23 10:29:37 -0500

    Create ExchangeContract.java

---
+++ src/example/ExchangeContract.java
@@ -0,0 +1,20 @@
+/* This example is for an exchange of real currency or non-Actum cryptocurrency with Actum tokens
+ * For an exchange on ActumCrypto, use an atomic exchange instead
+ */
+
+public class ExchangeContract extends SmartContract {
+  private boolean check() {
+    JSONObject json = JSONReader.readJsonFromUrl("https://yourdomain.com/");
+    if (json.get("sent").equals("true") {
+      BigDecimal otherAmount = new BigDecimal(Integer.parseInt(json.get("amount")));
+      BigDecimal exchangeRate = new BigDecimal(Integer.parseInt(json.get("rate")));
+      BigDecimal amount = otherAmount.multiply(exchangeRate);
+      balance = new Balance(amount, json.get("token");
+      rxAddress = json.get("rx");
+      return true;
+    } else if (json.get("cancel").equals("true") {
+      cancel = true;
+    }
+    return false;
+  }
+}
