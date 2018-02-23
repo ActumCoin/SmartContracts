bb8e1d9aed60776e367372b23de96fb089e960ed
rshanedev <ryansshane@rocketmail.com>
2018-02-23 10:32:32 -0500

    Update SmartContract.java

--- src/SmartContract.java
+++ src/SmartContract.java
@@ -1,12 +1,18 @@
 public class SmartContract {
   private boolean cancel;
+  private Balance balance = null;
+  private String txAddress = null;
+  private String rxAddress = null;
   
   private boolean check() {
     // where the smart contract is executed prior to payment
     return true;
   }
   
-  public boolean execute(Balance balance, String txAddress, String rxAddress) {
+  public boolean execute(Balance b, String t, String r) {
+    balance = balance != null ? balance : b;
+    txAddress = txAddress != null ? txAddress : t;
+    rxAddress = rxAddress != null ? rxAddress : r;
     while(!check()) {
       if (cancel) {
         return false;
